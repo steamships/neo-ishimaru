de1ed04ee30e856c490ac8894528a634a66867a2
大野洋平 <yohey.ono.ss@steamship@gmail.com>
2021-11-16 20:04:22 +0900

    ファイル分割、フォント設定など

---
+++ assets/js/_header.js
@@ -0,0 +1,5 @@
+export const header = () => {
+	const a = "犬";
+	const b = "ワン!";
+	console.log( a+ " " + b );
+}
--- assets/js/starter.js
+++ assets/js/starter.js
@@ -18,5 +18,10 @@
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+
+// import "../../node_modules/bootstrap/js/dist/util.js";
+// import "../../node_modules/bootstrap/js/dist/modal.js";
+
+//パーツごとのJSファイルを読み込み
+import { header } from "./_header.js";
+header();
