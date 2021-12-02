80ef0f491ab2f3382ac37d2bdc8dc731f06cb6c1
大野洋平 <yohey.ono.ss@steamship@gmail.com>
2021-12-02 11:35:59 +0900

    合流

--- src/js/_header.js
+++ src/js/_header.js
@@ -1,19 +1,61 @@
 export const header = () => {
-	console.log('header');
+	if ($('#header').length) {
+		console.log('header');
 
-	//ヘッダーの高さを取得→bodyのpadding-topへ設定
-	const headerHight = $('.header-nav').outerHeight();
-	$('body').css('padding-top', headerHight + 10 + "px");
+		//ヘッダーの高さを取得→bodyのpadding-topへ設定
+		const headerHight = $('.header-nav').outerHeight();
+		$('body').css('padding-top', headerHight + 10 + "px");
 
-	//ハンバーガーメニューの処理
-	$(".openbtn").click(function () {
-		$(this).toggleClass('active');
-	});
-
-	//
-	$('.main-nav .nav-item').hover(
-		function(){
-			$(this).find('.sub-nav').toggleClass('show');
+		//デバイスサイズでアクション分岐
+		const windowWidth = $(window).width();
+		const windowSm = 768;
+		if (windowWidth <= windowSm) {
+			$('.sub-nav').wrap('<div class="sub-nav-wrap">')
+			//クリックアクションでサブメニュー表示
+			$('.main-nav .nav-item').click(function(){
+				const $this = $(this);
+				$this.find('.sub-nav-wrap').toggle(400);
+				$this.find('.sub-nav').toggleClass('show');
+				$this.find('.nav-link').toggleClass('rotate');
+			});
+			//ハンバーガーメニューの処理
+			$(".openbtn").click(function () {
+				$(this).toggleClass('active');
+				$('#navbarNav').toggleClass('navScroll');
+			});
 		}
-	);
+		else {
+			//ホバーアクションでサブメニュー表示
+			$('.main-nav .nav-item').hover(
+				function(){
+					$(this).find('.sub-nav').toggleClass('show');
+				}
+			);
+		}
+
+		//検索オーバーレイ
+		$("#open-btn").click(function() {
+			$("#overlay").fadeIn(200);
+			$(".form-input").focus();
+		});
+		$(".close").click(function() {
+			$("#overlay").fadeOut(200);
+		});
+		// // バブリングを停止
+		// $(".overlay-inner").click(function(e){
+		// 	e.stopPropagation();
+		// });
+
+		// モーダルが開いたら、bodyにfixedを付与
+
+		// $('body').css('position','fixed');
+		// $('body').css('top',`-${window.scrollY}px`);
+
+		// // モーダルが閉じられ時...
+		// const top = document.body.style.top;
+		// $('body').css('position','');
+		// $('body').css('top','');
+		// document.body.style.top = '';
+		// window.scrollTo(0, parseInt(scrollY || '0') * -1);
+	}
 }
